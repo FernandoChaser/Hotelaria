af74b8d91d066024492356d67d2b4d9565f46473
Luiz Fernando Sant Anna da Silveira <luizfernandocnk@gmail.com>
2024-01-13 12:08:00 -0300

    Project Upload
    Criando menu do projeto de hotelaria

---
+++ Hotelaria.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h> 
+#include <string.h> 
+
+int main()
+ {
+ 	int opcao = 0;
+
+    setlocale(LC_ALL, "Portuguese");  //setar lingua
+
+	printf ("\t Sistema de reservas \n\n");
+	printf ("\t Escolha a opção desejada no menu: \n\n");
+	printf ("\t 1 -  Registro de usuarios \n\n ");
+	printf ("\t 2 -  Fazer Reserva\n\n ");
+	printf ("\t 3 -  Cancelar Reserva\n\n");
+	printf ("\t 4 -  Consultar informações de usuario \n\n ");
+	printf ("\t 5 -  Consultar informações de reseva \n\n ");
+    printf ("\t 6 -  Deletar registro de usuario \n\n");
+    printf ("\t 7 -  Sair do sistema \n\n");
+    
+    scanf ("%d",&opcao); 
+    system ("cls");
+    
+   
+
+    
+   
+ };
+ 
+ int consulta ()
+ {
+ 	
+ 	
+ };
+ 
+ int registro ()
+ {
+ 	
+ 	
+ };
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
